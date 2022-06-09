ab4b89e924b156bd71aac24defb6847c283cce1b
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2022-06-09 10:13:31 -0300

    Commented functions in ScriptsAuxiliares

--- ScriptsAuxiliares.rb
+++ ScriptsAuxiliares.rb
@@ -1,9 +1,11 @@
+# Função para iniciar as variáveis de time como 0
 def startGame
   for i in 27..33
     $game_variables[i] = 0
   end
 end
 
+# Função que adicionar um Pokemon na equipe adversária
 def colocarNaEquipeAdversaria(pkmn)
   for i in 27..33
     if $game_variables[i] == 0
@@ -17,6 +19,7 @@ def colocarNaEquipeAdversaria(pkmn)
   end
 end
 
+# Função para exluir toda a equipe adversária
 def excluirEquipeAdversaria
   for i in 27..33
     $game_variables[i] = 0
@@ -24,83 +27,57 @@ def excluirEquipeAdversaria
   pbMessage(_INTL("Equipe adversária excluída."))
 end
 
-def pbTrainerBattle(trainerID, trainerName, endSpeech=nil,
-                    doubleBattle=false, trainerPartyID=0, canLose=false, outcomeVar=1)
-  # Perform the battle
-  decision = pbTrainerBattleCore([trainerID,trainerName,trainerPartyID,endSpeech])
-  return (decision==1)
-end
-
+# Função para criação dos times e iniciar batalha
 def pbTrainerBattleCore(*args)
   outcomeVar = 1
-  canLose    = false
   Events.onStartBattle.trigger(nil)
+
+  # Criação do time do adversário
   foeTrainers    = []
   foeItems       = []
   foeEndSpeeches = []
   foeParty       = []
   foePartyStarts = []
   for arg in args
-    if arg.is_a?(NPCTrainer)
-      foeTrainers.push(arg)
-      foePartyStarts.push(foeParty.length)
-      arg.party.each { |pkmn| foeParty.push(pkmn) }
-      foeEndSpeeches.push(arg.lose_text)
-      foeItems.push(arg.items)
-    elsif arg.is_a?(Array)
       trainer = pbLoadTrainer(arg[0],arg[1],arg[2])
-      pbMissingTrainer(arg[0],arg[1],arg[2]) if !trainer
-      return 0 if !trainer
       Events.onTrainerPartyLoad.trigger(nil,trainer)
       foeTrainers.push(trainer)
       foePartyStarts.push(foeParty.length)
-
       for i in 27..33 do
         if $game_variables[i] != 0
           foeParty.push(Pokemon.new($game_variables[27],100))
         end
       end
-      # trainer.party.each { |pkmn| foeParty.push(pkmn) }
-      
       foeEndSpeeches.push(arg[3] || trainer.lose_text)
-      foeItems.push(trainer.items)
-    else
-      raise _INTL("Expected NPCTrainer or array of trainer data, got {1}.", arg)
-    end
   end
 
-  # Calculate who the player trainer(s) and their party are
+  # Criação do time do player
   playerTrainers    = [$Trainer]
   playerParty       = $Trainer.party
   playerPartyStarts = [0]
-  # Create the battle scene (the visual side of it)
+
+  # Criando cena de batalha
   scene = pbNewBattleScene
-  # Create the battle class (the mechanics side of it)
   battle = PokeBattle_Battle.new(scene,playerParty,foeParty,playerTrainers,foeTrainers)
   battle.party1starts = playerPartyStarts
   battle.party2starts = foePartyStarts
   battle.items        = foeItems
   battle.endSpeeches  = foeEndSpeeches
-  # Set various other properties in the battle class
   pbPrepareBattle(battle)
   $PokemonTemp.clearBattleRules
-  # End the trainer intro music
   Audio.me_stop
-  # Perform the battle itself
   decision = 0
+
+  # Iniciando batalha
   pbBattleAnimation(pbGetTrainerBattleBGM(foeTrainers),(battle.singleBattle?) ? 1 : 3,foeTrainers) {
     pbSceneStandby {
       decision = battle.pbStartBattle
     }
-    pbAfterBattle(decision,canLose)
+    pbAfterBattle(decision,false)
   }
+
+  # Retornando resultado da batalha (1 se tiver ganhado, 2 se tiver perdido)
   Input.update
-  # Save the result of the battle in a Game Variable (1 by default)
-  #    0 - Undecided or aborted
-  #    1 - Player won
-  #    2 - Player lost
-  #    3 - Player or wild Pokémon ran from battle, or player forfeited the match
-  #    5 - Draw
   pbSet(outcomeVar,decision)
   return decision
 end
