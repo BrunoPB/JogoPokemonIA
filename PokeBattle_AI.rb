5ec92b09af5934a0257524173f3f62324c9782e9
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2022-06-17 02:02:36 -0300

    Updated comments

--- PokeBattle_AI.rb
+++ PokeBattle_AI.rb
@@ -6,6 +6,7 @@ class PokeBattle_AI
     @battle = battle
   end
   
+  # Essa função é chamada pela função de batalha na biblioteca quando vai decidir a ação do computador
   # Função para decidir se deve trocar ou atacar. Tambem executa essas ações
   def pbDefaultChooseEnemyCommand(idxBattler)
       return if pbEnemyShouldWithdraw?(idxBattler)
